ad285c849016a9f040d4c3781a407394407d8cdc
Jose Daniel Jimenez Barahona <josedanielj@gmail.com>
2020-07-25 21:38:28 -0600

    Pantallas JDJ
    Pantallas:
    Menu principal con iconos
    Configuración
    Quienes comos
    Pantalla de inicio
    Sección principal
    Objetivo de ahorros
    Presupuesto
    Gráficas

---
+++ Saving_Helper/app/src/main/java/com/example/saving_helper/DetallesProgreso.java
@@ -0,0 +1,14 @@
+package com.example.saving_helper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetallesProgreso extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalles_progreso);
+    }
+}
---
+++ Saving_Helper/app/src/main/java/com/example/saving_helper/MenuConIconos.java
@@ -0,0 +1,13 @@
+package com.example.saving_helper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuConIconos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_con_iconos);
+    }
+}
